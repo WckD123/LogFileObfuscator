7cac2607234d5fe234c431b838ddf1a1c16a89a3
Mehul Kaushik <mkmehulkaushik@gmail.com>
2019-07-12 14:23:05 +0530

    Added a static home page route. Created a view/index.html file for static content

--- src/main.go
+++ src/main.go
@@ -4,14 +4,18 @@ import (
 	//"net/http"
 	//"strconv"
 
+	"github.com/gin-gonic/contrib/static"
+	_ "github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	//"net/http"
 )
 
 var db *gorm.DB
 
 func init() {
+
 	//open a db connection
 	var err error
 	db, err = gorm.Open("mysql", "root:password@/logger?charset=utf8&parseTime=True&loc=Local")
@@ -24,8 +28,20 @@ func init() {
 }
 
 func main() {
+
 	router := gin.Default()
 
-	router.Run()
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	api := router.Group("/api")
+	{
+		api.GET("/", apiHome)
+	}
+
+	router.Run(":3000")
+
+}
+
+func apiHome(c *gin.Context){
 
 }
