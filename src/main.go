202b2af0f620c26f291e085ec2f30623b225a478
Mehul Kaushik <mkmehulkaushik@gmail.com>
2019-07-15 12:54:56 +0530

    Restructured and added uploadHelper which uses GoRoutines to insert data into the DB.

--- src/main.go
+++ src/main.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-	//"net/http"
-	//"strconv"
-
 	_ "database/sql"
-	_"encoding/json"
-	_"fmt"
+	_ "encoding/json"
+	"fmt"
+	_ "fmt"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	_"io/ioutil"
-	_"os"
+	_ "io/ioutil"
+	"time"
 	_ "time"
-
 )
 
 var db *gorm.DB
 
 func init() {
 
-	//open a db connection
+	// CREATE DB CONNECTION
 	var err error
 	db, err = gorm.Open("mysql", "root:password@/logger?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	//Migrate the schema
-	db.AutoMigrate(&Logfile{}, &Logfile2{}, &Logfile3{})
+	// AUTOMIGRATE THE SCHEMA
+	db.AutoMigrate(&Logfile{})
 }
+
+// DECLARING MODEL
 
 type Logfile struct {
 	gorm.Model
@@ -42,7 +41,6 @@ type Logfile struct {
 	Num          int  `json:"num"`
 	Address      string `json:"address"`
 }
-
 
 func main() {
 
@@ -62,24 +60,43 @@ func main() {
 
 }
 
+// UPLOAD FUNCTION (PARSE JSON FILES AND INSERT IN DB)
+
 func upload(c *gin.Context){
 
 	var logfiles []Logfile
 	c.BindJSON(&logfiles)
 
-	for i := 0; i< len(logfiles) ; i++ {
-		error := db.Create(&logfiles[i]).Error
+	jobs := make(chan int, 100)
+
+	for w := 1; w <= 3; w++ {
+		go uploadHelper(logfiles, jobs)
+	}
+
+	for j := 0; j < len(logfiles); j++ {
+		jobs <- j
+	}
+	close(jobs)
+}
+
+func uploadHelper(logfiles []Logfile, jobs <-chan int){
+	for j := range jobs {
+		fmt.Println("worker started  job", j)
+		error := db.Create(&logfiles[j]).Error
 		if  error != nil {
 			panic(error)
 		}
+		fmt.Println("worker finished job", j)
 	}
-
 }
+
+// FUNCTION TO GET USER RECORDS AS USER (OBFUSCATED RECORDS)
 
 func viewAsUser(c *gin.Context){
 	id := c.Params.ByName("id")
 	key := c.Params.ByName("key")
 	var logfile []Logfile
+
 
 	db.Where("" + key + " = ?", id).Find(&logfile)
 
@@ -91,6 +108,7 @@ func viewAsUser(c *gin.Context){
 	c.JSON(200, logfile)
 }
 
+// FUNCTION TO GET USER RECORDS AS ADMIN
 
 func viewAsAdmin(c *gin.Context){
 	id := c.Params.ByName("id")
